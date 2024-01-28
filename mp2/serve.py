f9400841095b355283ff1395ff16aca5e7fcd3c0
Jing <jing@jings-mac.home>
2024-01-28 15:30:59 -0500

    step-2 files added

--- mp2/serve.py
+++ mp2/serve.py
@@ -1,17 +1,22 @@
+from flask import Flask, jsonify
 import subprocess
 import socket
-from flask import Flask, request
+
 app = Flask(__name__)
 
+# HTTP POST to trigger CPU stress
+@app.route('/', methods=['POST'])
+def stress_cpu():
+    # Start stress_cpu.py as a subprocess
+    subprocess.Popen(["python3", "stress_cpu.py"])
+    return jsonify(success=True, message="CPU stress started")
 
-@app.route('/', methods=['POST', 'GET'])
-def main():
-    global seed
-    if request.method == 'POST':
-        subprocess.run(["python3", "stress-cpu.py"])
-    else:
-        return socket.gethostname()
-
+# HTTP GET to return the private IP address
+@app.route('/', methods=['GET'])
+def get_ip():
+    hostname = socket.gethostname()
+    private_ip = socket.gethostbyname(hostname)
+    return jsonify(ip_address=private_ip)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- mp2/submit.py
+++ mp2/submit.py
@@ -4,18 +4,18 @@ import json
 ''' Fill in the following information '''
 # General information
 YOUR_EMAIL = "xx33@illinois.edu"  # <put your coursera account email>,
-YOUR_SECRET = "RKFc11mdo0AMncQr"  # <put your secret token from coursera>
+YOUR_SECRET = "UQ3iPfopHjw43Wlb"  # <put your secret token from coursera>
 
 # Section 1
 # <put your first EC2 instance's IP address:port>
-IP_ADDRESS1 = "18.206.99.196:5000"
+IP_ADDRESS1 = "18.224.60.58:5000"
 # <put your second instance's IP address:port>
-IP_ADDRESS2 = "100.24.27.52:5000"
+IP_ADDRESS2 = "52.15.106.42:5000"
 # <put your load_balancer address for section 1>
-YOUR_LOAD_BALANCER1 = "lb-838430069.us-east-1.elb.amazonaws.com"
+YOUR_LOAD_BALANCER1 = "cs498-mp2-288155995.us-east-2.elb.amazonaws.com:5000"
 # Section 2
 # <put your load_balancer address for section 2>,
-YOUR_LOAD_BALANCER2 = "internal-mp2v2-lb2-176400029.us-east-1.elb.amazonaws.com"
+YOUR_LOAD_BALANCER2 = "cs498-mp2-acp-1070771599.us-east-2.elb.amazonaws.com:5000"
 
 ''' Don't change the following '''
 url = "https://ekwygde36j.execute-api.us-east-1.amazonaws.com/alpha/execution"
